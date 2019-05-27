8182c209fefcf6f883835649f20331fd4f3da1ac
AmielYanez <amielsinue@gmail.com>
2019-05-27 13:10:07 -0500

    - adding docker-compose

--- schema.js
+++ schema.js
@@ -8,94 +8,93 @@ import {
   GraphQLList,
   GraphQLSchema
 } from 'graphql'
-import Db from '/db';
+import Db from './db';
 
 
 const Person = new GraphQLObjectType({
-  name: 'Person',
-  description: 'This represents a Person',
-  fields: () => {
-    return {
-      id: {
-        type: GraphQLInt,
-        resolve(person) {
-          return person.id;
+    name: 'Person',
+    description: 'This represents a Person',
+    fields: () => {
+        return {
+            id: {
+                type: GraphQLInt,
+                resolve(person) {
+                    return person.id;
+                }
+            },
+            firstName: {
+                type: GraphQLString,
+                resolve(person){
+                    return person.firstName
+                }
+            },
+            lastName: {
+                type: GraphQLString,
+                resolve(person){
+                    return person.lastName
+                }
+            },
+            email: {
+                type: GraphQLString,
+                resolve(person){
+                    return person.email
+                }
+            }
         }
-      },
-      firstName: {
-        type: GraphQLString,
-        resolve(person){
-          return person.firstName
-        }
-      },
-      lastName: {
-        type: GraphQLString,
-        resolve(person){
-          return person.lastName
-        }
-      },
-      email: {
-        type: GraphQLString,
-        resolve(person){
-          return person.email
-        }
-      }
     }
-  }
 });
-
-const Post = GraphQLObjectType({
-  name: 'Post',
-  description: 'This is a Post',
-  fields: () => {
-    return {
-      id: {
-        type: GraphQLInt,
-        resolve(post) {
-          return post.id;
+const Post = new GraphQLObjectType({
+    name: 'Post',
+    description: 'This is a Post',
+    fields: () => {
+        return {
+            id: {
+                type: GraphQLInt,
+                resolve(post) {
+                    return post.id;
+                }
+            },
+            title: {
+                type: GraphQLString,
+                resolve(post){
+                    return post.title
+                }
+            },
+            content: {
+                type: GraphQLString,
+                resolve(post){
+                    return post.content
+                }
+            }
         }
-      },
-      title: {
-        type: GraphQLString,
-        resolve(post){
-          return post.title
-        }
-      },
-      content: {
-        type: GraphQLString,
-        resolve(post){
-          return post.content
-        }
-      }
     }
-  }
 });
 
 const Query = new GraphQLObjectType({
-  name: 'Query',
-  description: 'This is a root query',
-  fields: () =>{
-    return {
-      people: {
-        type: GraphQLList(Person),
-        args:{
-          id: {
-            type: GraphQLInt,
-          },
-          email: {
-            type: GraphQLString
-          }
-        },
-        resolve(root, args) {
-          return Db.models.person.findAll({where: args});
+    name: 'Query',
+    description: 'This is a root query',
+    fields: () =>{
+        return {
+            people: {
+                type: GraphQLList(Person),
+                args:{
+                    id: {
+                        type: GraphQLInt,
+                    },
+                    email: {
+                        type: GraphQLString
+                    }
+                },
+                resolve(root, args) {
+                    return Db.models.person.findAll({where: args});
+                }
+            }
         }
-      }
     }
-  }
 });
 
 const Schema = new GraphQLSchema({
-  query: Query
+    query: Query
 });
 
 
--- server.js
+++ server.js
@@ -7,3 +7,16 @@ import GraphHTTP from 'express-graphql';
 import Schema from './schema';
 
 
+const APP_PORT = 3000;
+
+const app = Express();
+
+app.use('/graphql', GraphHTTP({
+    schema: Schema,
+    pretty: true,
+    graphiql: true
+}));
+
+app.listen(APP_PORT, ()=>{
+    console.log(`App listening on port ${APP_PORT}`)
+})
